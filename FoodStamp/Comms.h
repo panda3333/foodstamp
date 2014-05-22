65f69cd58f98ac5ad6d227de629b7e0caa163c4c
Lidgardo <chuynadamas@gmail.com>
2014-05-21 20:19:36 -0500

    Add LogInViewController
    Added the LogInScreen.

---
+++ FoodStamp/Comms.h
@@ -0,0 +1,16 @@
+//
+//  Comms.h
+//  FoodStamp
+//
+//  Created by Jesus Cruz Perez on 01/04/14.
+//  Copyright (c) 2014 Toby Stephens. All rights reserved.
+//
+
+@protocol CommsDelegate <NSObject>
+@optional
+   - (void) commsDidLogin:(BOOL)loggedIn;
+@end
+
+@interface Comms : NSObject
+   + (void) login:(id<CommsDelegate>)delegate;
+@end
